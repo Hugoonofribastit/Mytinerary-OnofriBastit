f62700b933ad3a7359a0560e884581312594d0ac
Hugo Onofri Bastit <74626682+Hugoonofribastit@users.noreply.github.com>
2022-02-26 03:06:41 -0300

    backend add

---
+++ src/components/datoscards.js
@@ -0,0 +1,133 @@
+const datoscards =[
+    {
+      
+        "id": 1,
+        "image":"tokyo.jpg",
+        "name": "Tokyo",
+        "description":"Tokyo (東京, Tōkyō) is Japan's capital and the world's most populous metropolis. It is also one of Japan's 47 prefectures, consisting of 23 central city wards and multiple cities, towns and villages west of the city center. The Izu and Ogasawara Islands are also part of Tokyo. Prior to 1868, Tokyo was known as Edo. A small castle town in the 16th century, Edo became Japan's political center in 1603 when Tokugawa Ieyasu established his feudal government there. A few decades later, Edo had grown into one of the world's most populous cities. With the Meiji Restoration of 1868, the emperor and capital moved from Kyoto to Edo, which was renamed Tokyo (Eastern Capital). Large parts of Tokyo were destroyed in the Great Kanto Earthquake of 1923 and in the air raids of 1945. Today, Tokyo offers a seemingly unlimited choice of shopping, entertainment, culture and dining to its visitors. The city's history can be appreciated in districts such as Asakusa and in many excellent museums, historic temples and gardens. Contrary to common perception, Tokyo also offers a number of attractive green spaces in the city center and within relatively short train rides at its outskirts.",
+        
+    },
+
+    {
+      "id": 2,
+      "image":"hiroshima.jpg",
+      "name": "Hiroshima",
+      "description":"Hiroshima (広島) is the principal city of the Chugoku Region and home to over a million inhabitants. When the first atomic bomb was dropped over Hiroshima on August 6, 1945, the city became known worldwide for this unenviable distinction. The destructive power of the bomb was tremendous and obliterated nearly everything within a two kilometer radius. After the war, great efforts were taken to rebuild the city. Predictions that the city would be uninhabitable proved false. Destroyed monuments of Hiroshima's historical heritage, like Hiroshima Castle and Shukkeien Garden, were reconstructed. In the center of the city a large park was built and given a name that would reflect the aspirations of the re-born city: Peace Memorial Park.",
+   
+     
+    },
+
+    {
+        "id": 3,
+        "image":"fukushima.jpg",
+        "name": "Fukushima",
+        "description":"Fukushima (福島) is the capital city of Fukushima Prefecture in the Tohoku Region of Japan. Located along the foothills of the Azuma mountain range about 40 kilometers inland from the Pacific Ocean, Fukushima is not a major tourist destination, but offers a variety of natural and seasonal attractions including hot springs, hiking trails in spectacular volcanic landscapes, and Hanamiyama, one of Japan's most pleasant cherry blossom spots.",
+    },
+
+    {
+      
+        "id": 4,
+        "image":"kioto.jpg",
+        "name": "Kioto",
+        "description":"Kyoto (京都, Kyōto) served as Japan's capital and the emperor's residence from 794 until 1868. It is one of the country's ten largest cities with a population of 1.5 million people and a modern face. Over the centuries, Kyoto was destroyed by many wars and fires, but due to its exceptional historic value, the city was dropped from the list of target cities for the atomic bomb and escaped destruction during World War II. Countless temples, shrines and other historically priceless structures survive in the city today.",
+    },
+
+    {
+      
+        "id": 5,
+        "image":"kobe.jpg",
+        "name": "Kobe",
+        "description":"Kobe (神戸, Kōbe) is the capital of Hyogo Prefecture and one of Japan's ten largest cities. Located between the sea and the Rokko mountain range, Kobe is also considered one of Japan's most attractive cities. Kobe has been an important port city for many centuries. Its port was among the first to be opened to foreign trade in the 19th century alongside the ports of Yokohama, Nagasaki, Hakodate and Niigata.",
+       
+    },
+
+    {
+    "id": 6,
+    "image":"nagano.jpg",
+    "name": "Nagano",
+    "description":"Nagano City (長野) is the capital of Nagano Prefecture. It evolved as a temple town around Zenkoji, one of Japan's most popular temples. In 1998, the city hosted the Winter Olympic Games, and some former olympic facilities can still be viewed around town. In the forested mountains northwest of the city center lies the Togakushi area which attracts tourists with its popular shrines and as the legendary home of the Togakure Ninja School. The Togakushi Ninpo Museum and the Kids Ninja Village provide good entertainment and a glimpse of the area's ninja past.",
+        
+    },
+
+    {
+      
+        "id": 7,
+        "image":"nara.jpg",
+        "name": "Nara",
+        "description":"Nara (奈良県, Nara-ken), a landlocked prefecture in the Kansai Region, is the cradle of the Japanese civilization. Many of the early emperors built their palaces in Nara Prefecture, formerly known as Yamato. In 710, Nara was selected as the location of Japan's first permanent capital, Heijo (today's Nara City). Previously, the capital was moved with the ascension of each new emperor.",
+    },
+
+    {
+      
+        "id": 8,
+        "image":"okinawa.jpg",
+        "name": "Okinawa",
+        "description":"Okinawa (沖縄県, Okinawa-ken) is Japan's southernmost prefecture, consisting of a few dozen, small islands in the southern half of the Nansei Shoto, the island chain which stretches over about one thousand kilometers from Kyushu to Taiwan. Okinawa Prefecture can be divided into three major island groups, the Okinawa Islands (Okinawa Shoto) around Okinawa Island (Okinawa Honto), the Miyako Islands (Miyako Retto) around Miyako Island and the Yaeyama Islands (Yaeyama Retto) around Ishigaki Island.",
+        
+    },
+
+    {
+      
+        "id": 9,
+        "image":"sapporo.jpg",
+        "name": "Sapporo",
+        "description":"Sapporo (札幌) is the capital of Hokkaido and Japan's fifth largest city. Sapporo is also one of the nation's youngest major cities. In 1857, the city's population stood at just seven people.In the beginning of the Meiji Period, when the development of Hokkaido was started on a large scale, Sapporo was chosen as the island's administrative center and enlarged according to the advice of foreign specialists. Consequently, Sapporo was built based on a rectangular street system.",
+        
+    },
+
+    {
+      
+        "id": 10,
+        "image":"osaka.jpg",
+        "name": "Osaka",
+        "description":"Osaka (大阪, Ōsaka) is Japan's second largest metropolitan area after Tokyo. It has been the economic powerhouse of the Kansai Region for many centuries. Osaka was formerly known as Naniwa. Before the Nara Period, when the capital used to be moved with the reign of each new emperor, Naniwa was once Japan's capital city, the first one ever known. In the 16th century, Toyotomi Hideyoshi chose Osaka as the location for his castle, and the city may have become Japan's political capital if Tokugawa Ieyasu had not terminated the Toyotomi lineage after Hideyoshi's death and moved his government to distant Edo (Tokyo).",
+        
+    },
+
+    {
+      
+        "id": 11,
+        "image":"obanazawa.jpg",
+        "name": "Obanazawa",
+        "description":"Ginzan Onsen (銀山温泉) in Obanazawa is a secluded hot spring town nestled in the mountains of Yamagata Prefecture. The area originally developed around a silver mine; however, these days it is better known as one of Japan's prettiest onsen towns with historic ryokan lined up along the river. Ginzan Onsen's traditionally styled ryokan occupy beautiful three and four story wooden buildings that, with their exposed woodwork and white plaster walls, evoke nostalgic feelings of a bygone era. There is one exception, however, in the Fujiya ryokan at the center of town, which was recently rebuilt by famous modern architect Kuma Kengo. Although purists may find it out of place, its unique modern design incorporates many of the traditional design elements, such as the same exposed wood and white plaster, of the surrounding buildings.",
+        
+    },
+    
+    {
+      
+        "id": 12,
+        "image":"nagoya.jpg",
+        "name": "Nagoya",
+        "description":"With over two million inhabitants, Nagoya (名古屋) is Japan's fourth most populated city. It is the capital of Aichi Prefecture and the principal city of the Nobi plain, one of Honshu's three large plains and metropolitan and industrial centers. Nagoya developed as the castle town of the Owari, one of the three branches of the ruling Tokugawa family during the Edo Period. Much of the city, including most of its historic buildings, were destroyed in the air raids of 1945. The Toyota Motor Corporation maintains its headquarters just outside of Nagoya.",
+        
+    },
+    
+    {
+      
+        "id": 13,
+        "image":"fukuoka.jpg",
+        "name": "Fukuoka",
+        "description":"Fukuoka (福岡) is Kyushu's largest and one of Japan's ten most populated cities. Because of its closeness to the Asian mainland (closer to Seoul than to Tokyo), Fukuoka has been an important harbor city for many centuries and was chosen by the Mongol invasion forces as their landing point in the 13th century. Today's Fukuoka is the product of the fusion of two cities in the year 1889, when the port city of Hakata and the former castle town of Fukuoka were united into one city called Fukuoka. Hakata remains the name of one of Fukuoka's central districts and of the main railway station.",
+        
+    },
+    {
+      
+        "id": 14,
+        "image":"yokohama.jpg",
+        "name": "Yokohama",
+        "description":"Yokohama (横浜) is Japan's second largest city with a population of over three million. Yokohama is located less than half an hour south of Tokyo by train, and is the capital of Kanagawa Prefecture. Towards the end of the Edo Period (1603-1867), during which Japan maintained a policy of self-isolation, Yokohama's port was one of the first to be opened to foreign trade in 1859. Consequently, Yokohama quickly grew from a small fishing village into one of Japan's major cities. Until today, Yokohama remains popular among expats, has one of the world's largest Chinatowns and preserves some former Western residences in the Yamate district.",
+    },
+    {
+      
+        "id": 15,
+        "image":"hakodate.jpg",
+        "name": "Hakodate",
+        "description":"Hakodate (函館) is Hokkaido's third largest city, located at the island's southern tip. Hakodate is best known for the spectacular views to be enjoyed from Mount Hakodate and its delicious, fresh seafood. As one of the first Japanese harbor cities to be opened to international trade after the country's era of isolation, Hakodate has experienced notable influence from overseas, and the foreign population's former residential district and a Western style fort are among its main tourist attractions. Onuma Park, a quasi national park with beautiful, island dotted lakes, is located only half an hour north of Hakodate and makes a nice side trip or a stop along the journey between Hakodate and Sapporo.",
+        
+    },
+
+    
+  
+  ]
+
+  export default datoscards;
