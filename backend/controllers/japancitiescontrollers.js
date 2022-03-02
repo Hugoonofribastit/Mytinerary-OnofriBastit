94a8516e2777a2af722a58f05a8a1266a0345b18
Hugo Onofri Bastit <74626682+Hugoonofribastit@users.noreply.github.com>
2022-03-02 10:48:22 -0300

    controllers

--- backend/controllers/japancitiescontrollers.js
+++ backend/controllers/japancitiescontrollers.js
@@ -1,4 +1,7 @@
 
+
+
+
 const Ciudades = require("../models/japancities");
 
 const citiesControllers = {
@@ -17,5 +20,49 @@ const citiesControllers = {
       error: error,
     });
   },
-};
+
+
+  agregarCiudad: async (required, response) => {
+    console.log(required.body);
+    const { name, image, description } = required.body
+    new Ciudades({
+        name, image, description
+    }).save()
+        .then((respuesta) => response.json({ respuesta }))
+        .catch(error => response.json({ error }))
+},
+
+borrarCiudad: async (required, response) => {
+    const id = required.params.id
+
+    let ciudadEliminada
+
+    ciudadEliminada = await Ciudades.findOneAndDelete({ _id: id })
+        .then((res) => response.json({ paso: "listo", respuesta: res }))
+        .catch(error => response.json({ error }))
+},
+
+modificarCiudad: async (req, res) => {
+    const id = req.params.id
+    const ciudad = req.body
+
+    let ciudadb
+    ciudadb = await Ciudades.findOneAndUpdate({ _id: id }, ciudad, { new: true })
+        .then((response) => res.json({ paso: "listo", respuesta: response }))
+        .catch(error => res.json({ error }))
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = citiesControllers;
