9aa392eea074a0a6d8d42d27f6c13046bd164e4f
Hugo Onofri Bastit <74626682+Hugoonofribastit@users.noreply.github.com>
2022-03-02 09:49:53 -0300

    rutas viejas

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,8 +2,11 @@ const Router = require(`express`).Router();
 
 const ciudadesControllers = require(`../controllers/japancitiescontrollers`);
 
-const { consultarCiudades } = ciudadesControllers;
+const { consultarCiudades} = ciudadesControllers;
 
 Router.route(`/alljpcities`).get(consultarCiudades);
+
+
+
 
 module.exports = Router;
