df0dd03a4230a2de555fc900baed6faba7cfb932
Hugo Onofri Bastit <74626682+Hugoonofribastit@users.noreply.github.com>
2022-03-02 10:49:19 -0300

    routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,11 +2,11 @@ const Router = require(`express`).Router();
 
 const ciudadesControllers = require(`../controllers/japancitiescontrollers`);
 
-const { consultarCiudades, agregarCiudad, borrarCiudad, modificarCiudad } = ciudadesControllers;
+const { consultarCiudades, agregarCiudad, borrarCiudad,modificarCiudad} = ciudadesControllers;
 
 Router.route(`/alljpcities`).get(consultarCiudades).post(agregarCiudad);
 
-Router.route(`/alljpcities/:id`).delete(borrarCiudad).put(modificarCiudad);
+Router.route(`/alljpcities/id:`).delete(borrarCiudad).put(modificarCiudad)
 
 
 module.exports = Router;
